050053aa8ce0cec53a9d460c4f93978b476f665b
Elliot Weaver <elliot.weaver@gmail.com>
2014-06-29 17:39:57 -0700

    almost done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,45 @@
 (function($) {
 
+  $.localScroll();
+
   // Gallery
   var $packery = $('#packery');
   $packery.packery({
     itemSelector: '.item',
     gutter: 10
+  });
+
+  setInterval(function(){
+    $packery.packery({
+      itemSelector: '.item',
+      gutter: 10
+    });
+  },500);
+
+  var $item = $(".page-gallery .item");
+  $item.on('click', function(){
+    $pop = $('.pop', this);
+    if($pop.hasClass('active')){
+
+    }
+    else {
+      $pop.addClass('active');
+      $pop.animate({
+        top: '0px'
+      },500, function(){
+
+      });
+    }
+  });
+
+  var $close = $(".page-gallery .pop .close");
+  $close.on('click', function(){
+    $parent = $(this).parent();
+    $parent.animate({
+      top: '-1000px'
+    },500, function(){
+      $parent.removeClass('active');
+    });
   });
 
 }(jQuery));
