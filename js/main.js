ee2155067545fe84af679beaa379f57f18b22813
Elliot Weaver <elliot.weaver@gmail.com>
2014-08-12 08:21:49 -0700

    image fixes

--- js/main.js
+++ js/main.js
@@ -16,30 +16,59 @@
     });
   },500);
 
+  // Activate popup
   var $item = $(".page-gallery .item");
   $item.on('click', function(){
-    $pop = $('.pop', this);
-    if($pop.hasClass('active')){
-
+    console.log(1);
+    $(".page-gallery .item .pop").hide();
+    $closing = $(".page-gallery .item .closing");
+    if($closing.length){
+      // do nothing
+      $closing.removeClass("closing");
     }
     else {
-      $pop.addClass('active');
-      $pop.animate({
-        top: '0px'
-      },500, function(){
-
-      });
+      $pop = $('.pop', this);
+      $pop.addClass('active').show();
     }
   });
 
+  // Close popup
   var $close = $(".page-gallery .pop .close");
   $close.on('click', function(){
-    $parent = $(this).parent();
-    $parent.animate({
-      top: '-1000px'
-    },500, function(){
-      $parent.removeClass('active');
-    });
+    console.log(2);
+    //$parent = $(this).parent();
+    //$parent.removeClass('active').hide();
+    $close.addClass("closing");
   });
+
+  // Resize popup
+  setInterval(function(){
+    var screenWidth = $(window).width();
+    var screenHeight = $(window).height();
+
+    if(screenWidth < 900){
+      var boxWidth = screenWidth - 20;
+      var boxHeight = screenHeight - 20;
+    }
+    else {
+      var boxWidth = screenWidth * .7;
+      var boxHeight = screenHeight * .85;
+    }
+
+
+    var marginLeft = -1 * (boxWidth / 2);
+    var marginTop = -1 * (boxHeight / 2);
+    $pop = $(".page-gallery .item .pop.active");
+    if($pop.length){
+      $pop.css({
+        width: boxWidth + 'px',
+        height: boxHeight + 'px',
+        "margin-left": marginLeft + 'px',
+        "margin-top": marginTop + 'px',
+        top: "50%",
+        left: "50%"
+      })
+    }
+  },10);
 
 }(jQuery));
